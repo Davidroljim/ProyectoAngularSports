e1ee84378043b06db317b39cedbeb5b61fe99cdb
davidroljim <david.roldan.jimenez.al@iespoligonosur.org>
2022-02-28 22:50:30 +0100

    proyecto

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -7,11 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./lista.component.css']
 })
 export class ListaComponent implements OnInit {
-
+  clasificacion:any;
   constructor( private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://www.thesportsdb.com/api/v1/json/2/lookuptable.php?l=4335&s=2021-2022').subscribe(data=>{console.log(data);});
+    this.http.get('https://www.thesportsdb.com/api/v1/json/2/lookuptable.php?l=4335&s=2021-2022').subscribe(data=>{this.clasificacion=data;});
+
+    console.table(this.clasificacion);
   }
 
+  
 }
